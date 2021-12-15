3c9e8d9b463e8e564de91177a47be37d6ee59c5e
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-15 16:21:01 +0530

    Getting products from database
    Getting products from database

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+const Product = require("./models/products.model")
+
 app.use(express.json())
 app.use(express.static("public"))
 app.set("view engine" , "ejs")
@@ -27,13 +29,51 @@ app.get("/auth/google/failure", function (req, res) {
     return res.send("Something went wrong")
 })
 
+app.get("/getapp", (req, res) => {
+
+    return res.render("getapp")
+
+})
 
 
+app.get("/help", (req, res) => {
+    return res.render("help")
+})
+app.get("/ProductNykaaCosmetics",async (req, res) => {
+    const product = await Product.find()
+    return res.render("ProductNykaaCosmetics")
+})
 
+app.get("/all", async (req, res) => {
+    const products = await Product.find().limit(15)
+   return res.send(products)
+})
 
+app.get("/Myprofile", (req, res) => {
+    return res.render("Myprofile")
+})
 
+app.get("/ProductDetail", (req, res) => {
+    return res.render("ProductDetail")
+})
+app.get("/success", (req, res) => {
+    return res.render("sucess")
+})
+app.get("/address", (req, res) => {
+    return res.render("address")
+})
 
+app.get("/payment", (req, res) => {
+    return res.render("payment")
+})
 
+app.get("/Mywish", (req, res) => {
+    return res.render("Mywish")
+})
+
+app.get("/offer", (req, res) => {
+    return res.render("offer")
+})
 
 
 
@@ -49,3 +89,9 @@ app.use("/prod", productcontroller)
 module.exports = app
 
 
+
+
+
+
+
+//db.collectionName.find({$text:{$search:”\”Phrase”\”}})
