09d9b3deeae6d7e69f29151a0c037c7ac2ffc46f
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-30 16:05:39 +0530

    deploy

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -6,13 +6,7 @@ router.post("/", async (req, res) => {
 
    const  user = await User.create(req.body)
     return res.status(201).send(user)
-    // let user = await User.findOne({email : req.body.email})
-    // if(user) {
-    //     return res.status(201).send(user)
-    // }else {
-    //  user = await User.create(req.body)
-    // return res.status(201).send(user)
-    // }
+    
 })
 
 router.get("/", async (req, res) => {
